46de1f1f817f298caa84840147cbd6c873c7e0c9
ealhl <yayt.tuk@gmail.com>
2023-12-09 14:45:47 -0800

    typewriter fixed

--- typewriter.js
+++ typewriter.js
@@ -1,7 +1,7 @@
 const sentence = "hello there from lighthouse labs";
-const sentenceTest = "ahshddgfhfjgjgkgk gjjf";
+const sentenceTest = "hello there from lighthouse labs";
 
-let time = -50;
+let time = 0;
 for (const char of sentenceTest) {
   setTimeout(() => {
     // print the char here
@@ -9,3 +9,7 @@ for (const char of sentenceTest) {
     process.stdout.write(char);
   }, time); // <= 1s delay to make it noticeable. Can dial it down later.
 }
+setTimeout(() => {
+  // print the char here
+  console.log("\n");
+}, time);
